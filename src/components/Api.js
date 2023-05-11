6a7c1c8031369fd5054cb8cfe349927cd372724f
tranduynam2904 <tranduynam2904@gmail.com>
2023-05-11 21:17:37 +0700

    first commit

---
+++ src/components/Api.js
@@ -0,0 +1,8 @@
+import { axios } from "axios"
+import { useState } from "react"
+export default function Api() {
+    const [data,setData] = useState(null)
+    const getData = async() => {
+
+    }
+}
